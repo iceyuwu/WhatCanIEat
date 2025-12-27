0951419fd95de6cd7df73f140f6c4f5e5d39b84e
yangyang <15989111595@163.com>
2025-12-27 20:10:25 +0800

    chore 初始化项目

---
+++ src/main/java/com/yangyang/whatcanieat/WhatCanIEatApplication.java
@@ -0,0 +1,13 @@
+package com.yangyang.whatcanieat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatCanIEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatCanIEatApplication.class, args);
+    }
+
+}
