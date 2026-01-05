eff65f7ea36cc11d3b7db68cad1c3a51dbeafa53
honmono <chihonmono@163.com>
2026-01-05 12:24:29 +0800

    feat 新增SpringAI依赖和接口

---
+++ src/main/java/com/yangyang/whatcanieat/controller/AiController.java
@@ -0,0 +1,42 @@
+package com.yangyang.whatcanieat.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ai")
+public class AiController {
+
+    private final ChatClient chatClient;
+
+    public AiController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam String prompt) {
+        return chatClient.prompt()
+                .user(prompt)
+                .call()
+                .content();
+    }
+
+    @GetMapping("/chat2")
+    public String chat2(@RequestParam String prompt) {
+        return chatClient.prompt()
+                .system("你是一个资深 Java 后端架构师")
+                .user(prompt)
+                .call()
+                .content();
+    }
+
+//    @GetMapping("/chat3")
+//    public ChatResponse chat3(@RequestParam String prompt) {
+//        return chatClient.prompt()
+//                .user(prompt)
+//                .call();
+//    }
+}
